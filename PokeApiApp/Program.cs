9de49eb30e86b5f0ab3af21c32333f5c18ca15ca
Kenneth Ordeneaux <ordeneauxk@outlook.com>
2024-10-19 05:27:12 -0500

    initial commit

---
+++ PokeApiApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokeApiNet;
+
+namespace PokeApiApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new PokeApiClient();
+            var p = client.GetResourceAsync<Pokemon>(1);
+            p.ConfigureAwait(false);
+            var pokemons = client.GetAllNamedResourcesAsync<Pokemon>();
+        }
+    }
+}
