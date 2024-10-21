cac0353df50388b5ae13d22fae4bb54b46cc6e83
Kenneth Ordeneaux <ordeneauxk@outlook.com>
2024-10-21 16:41:55 -0500

    basic i/o working

--- PokeApiApp/Program.cs
+++ PokeApiApp/Program.cs
@@ -1,20 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using PokeApiNet;
 
 namespace PokeApiApp
 {
     internal class Program
     {
+        private static string BASEURL = "https://pokeapi.co/api/v2/";
         static void Main(string[] args)
         {
-            var client = new PokeApiClient();
-            var p = client.GetResourceAsync<Pokemon>(1);
-            p.ConfigureAwait(false);
-            var pokemons = client.GetAllNamedResourcesAsync<Pokemon>();
+            //var client = new PokeApiClient();
+            //var p = client.GetResourceAsync<Pokemon>(1);
+            //p.ConfigureAwait(false);
+            //var pokemons = client.GetAllNamedResourcesAsync<Pokemon>();
+            //DataFetcher.
+            //var client = new DataFetcher();
+            Dictionary<string,Uri> map = new Dictionary<string,Uri>();
+            using (var httpClient = new HttpClient())
+            {
+                var pokeClient = new Client(httpClient);
+                //var charmander = pokeClient.Pokemon_retrieveAsync("4").Result;
+                var list = pokeClient.Pokemon_listAsync(int.MaxValue,0,string.Empty).Result;
+                foreach (var pokemon in list.Results.OrderBy(r => r.Name))
+                {
+                    map[pokemon.Name] = pokemon.Url;
+                }
+            }
+            bool validInput = false;
+            while (!validInput)
+            {
+                Console.WriteLine("Type a Pokemon name then Enter.");
+                var inputName = Console.ReadLine().Trim();
+                if (map.ContainsKey(inputName.ToLowerInvariant()))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid name.");
+                    Console.WriteLine("Valid names are:");
+                    foreach(string name in map.Keys)
+                    {
+                        Console.WriteLine(name);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
         }
     }
 }
